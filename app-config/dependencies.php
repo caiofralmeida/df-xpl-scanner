7823ff6686c93e8958269f5b1a6459e156b4eb50
Caio Almeida <caio.f.r.amd@gmail.com>
2016-07-27 21:34:01 -0300

    configura serviços no container

--- app-config/dependencies.php
+++ app-config/dependencies.php
@@ -4,8 +4,15 @@ use Pimple\Container;
 
 $container = new Container();
 
-$container->offsetSet('googleFinderProvider', function($container){
+$container->offsetSet('googleHtmlParser', function($container) {
+    return new CaioFRAlmeida\DfXplScanner\Finder\Parser\GoogleHtmlPage(
+        new \DOMDocument()
+    );
+});
+
+$container->offsetSet('googleFinderProvider', function($container) {
     return new CaioFRAlmeida\DfXplScanner\Finder\Google(
-        new GuzzleHttp\Client()
+        new GuzzleHttp\Client(),
+        $container['googleHtmlParser']
     );
 });
